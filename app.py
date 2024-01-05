88a44afc3a5887e9072ed24eee0fb35f37d760ff
Nilesh Gautam <nileshgautam13092001+git@gmail.com>
2024-01-05 13:50:44 +0530

    Added Form for each job and Data postint as well In this commit I have added forms for each job as well as posting data using post method, also created a function that handles empty strings

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db, load_job_from_db
+from flask import Flask, render_template, jsonify ,request
+from database import load_jobs_from_db, load_job_from_db, post_in_db
 app = Flask(__name__)
 
 JOBS = load_jobs_from_db()
@@ -7,7 +7,7 @@ JOBS = load_jobs_from_db()
 @app.route('/')
 def hello_world():
   # return 'hello world'
-  return render_template('home.html' , jobs = JOBS,project_name = "Flask")
+  return render_template('home.html' , jobs = JOBS)
 
 @app.route('/api/jobs')
 def list_jobs():
@@ -19,7 +19,16 @@ def job(id):
   # return jsonify(job)
   if not job:
     return render_template('notfound.html')
-  return render_template('jobpage.html', job = job)
+  return render_template('jobpage.html',job = job)
+
+@app.route("/job/<id>/submitted" , methods = ['post'])
+def submitted(id):
+  job = load_job_from_db(id)
+  data = request.form
+  post_in_db(data,id)
+  if not job:
+    return render_template('notfound.html')
+  return render_template('submitted.html' ,job = job,data = data)
 
 if __name__ == '__main__':  
   print('hello this is inside if statement')
--- database.py
+++ database.py
@@ -9,22 +9,19 @@ password = os.getenv('PASSWORD')
 host_name = os.getenv('HOST')
 db_name = os.getenv('DB')
 
-# print(user)
-
 my_secret = f'postgresql+psycopg2://{user}:{password}@{host_name}:5432/{db_name}'
 
 engine = create_engine(my_secret)
 
 def load_jobs_from_db():
     with engine.connect() as conn:
-        result = conn.execution_options(stream_results=True).execute(text("select * from jobs"))
+        result = conn.execution_options(stream_results = True).execute(text("select * from jobs"))
         result_all = result.all()
         result_dict = []
         for row in result_all:
             result_dict.append(dict(row._mapping))
         return (result_dict)
     
-# id = 3
 def load_job_from_db(id):
     with engine.connect() as conn:
         result = conn.execution_options(stream_results = True).execute(text(f'select * from jobs where id = {id}'))
@@ -32,3 +29,27 @@ def load_job_from_db(id):
         if len(result_all) == 0:
             return None
         return (dict(result_all[0]._mapping))
+    
+def none_handler(para):
+    for key in para:
+        if len(para[key]) == 0:
+            para[key] = None
+        print('hello para key : ',key)
+    return para
+
+
+def post_in_db(data,id):
+    with engine.connect() as conn:
+        para ={
+            'job_id' : id,
+            'name' : data.get('name'),
+            'mail' : data.get('mail'),
+            'phone' : data.get('phone'),
+            'education' : data.get('edu'),
+            'resume' : data.get('resume'),
+            'linkedin' : data.get('linkedin')
+        }
+        para = none_handler(para)  # for empty None that becomes null in postgresql rather than being empty string
+        query = text("insert into application (job_id, name, mail, phone, education, resume, linkedin) values (:job_id,:name,:mail,:phone,:education,:resume,:linkedin)")
+        result = conn.execute(query,para)
+        conn.commit()
